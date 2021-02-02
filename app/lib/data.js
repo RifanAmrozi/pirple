3d3d98e21507801c3a5c55fcbb4fc3113bea3195
Rifan Amrozi <rifan@archerninecloud.onmicrosoft.com>
2021-02-02 14:17:25 +0700

    initialization

---
+++ app/lib/data.js
@@ -0,0 +1,76 @@
+var fs = require('fs');
+var path = require('path')
+var helpers = require('./helpers')
+
+var lib ={}
+
+lib.baseDir = path.join(__dirname,'/../.data/');
+
+lib.create = function(dir,file,data,callback){
+    fs.open(lib.baseDir+dir+'/'+file+'.json','wx',function(err,fileDescriptor){
+        if(!err && fileDescriptor){
+            var stringData = JSON.stringify(data);
+            fs.writeFile(fileDescriptor,stringData,function(err){
+                if(!err){
+                    fs.close(fileDescriptor,function(err){
+                        if(!err){
+                            callback(false)
+                        }else{
+                            callback('Error closing new file')
+                        }
+                    })
+                }else{
+                    callback('Error writing to new file')
+                }
+            })
+        }else{
+            callback('Error create new file')
+        }
+    })
+}
+lib.read = function(dir,file,callback){
+    fs.readFile(lib.baseDir+dir+'/'+file+'.json','utf8',function(err,data){
+        if(!err && data){
+            var parsedData = helpers.parseJsonToObject(data)
+            callback(false,parsedData)
+        }else{
+            callback(err,data)
+        }
+    })
+}
+lib.update = function(dir,file,data,callback){
+    fs.open(lib.baseDir+dir+'/'+file+'.json','r+',function(err,fileDescriptor){
+        if(!err && fileDescriptor){
+            var stringData = JSON.stringify(data)
+            fs.truncate(fileDescriptor,function(err){
+                if(!err){
+                    fs.writeFile(fileDescriptor,stringData,function(err){
+                        if(!err){
+                            fs.close(fileDescriptor,function(err){
+                                if(!err){
+                                    callback(false)
+                                }else{
+                                    callback('Error closing existing file')
+                                }
+                            })
+                        }else{
+                            callback('Error writing to existing file')
+                        }
+                    })
+                }else{
+                    callback('Error truncating file')
+                }
+            })
+        }
+    })
+}
+lib.delete = function(dir,file,callback){
+    fs.unlink(lib.baseDir+dir+'/'+file+'.json',function(err){
+        if(!err){
+            callback(false);
+        }else{
+            callback('Error deleting file')
+        }
+    })
+}
+module.exports=lib
---
+++ app/lib/helpers.js
@@ -0,0 +1,81 @@
+var crypto = require('crypto')
+var config = require('./config')
+var https = require("https")
+var querystring = require("querystring")
+var helpers = {}
+
+helpers.hash = function(str){
+    if(typeof(str)=='string' && str.length>0){
+        var hash = crypto.createHmac('sha256',config.hashingSecret).update(str).digest('hex')
+        return hash
+    }else{
+        return false
+    }
+}
+
+helpers.parseJsonToObject = function(str){
+    try{
+        var obj = JSON.parse(str)
+        return obj
+    }catch(e){
+        return {}
+    }
+}
+
+helpers.createRandomString = function(strLength){
+    strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+    if(strLength){
+        var possibleCharacters = "qwertyuiopasdfghjklzxcvbnm1234567890";
+        var str = "";
+        for(let i=1;i<=strLength;i++){
+            var randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length))
+            str += randomCharacter
+        }
+        return str;
+    }else{
+        return false;
+    }
+}
+
+helpers.sendTwilioSms = function(phone,msg,callback){
+    phone = typeof(phone) == "string" && phone.trim().length == 10 ? phone.trim() : false;
+    msg = typeof(msg) == "string" && msg.trim().length > 0 && msg.trim().length < 1600 ? msg.trim() : false;
+    if(phone && msg){
+        var payload = {
+            "From" : config.twilio.fromPhone,
+            "To" : "+1"+phone,
+            "Body" : msg
+        }
+        var stringPayload = querystring.stringify(payload)
+        var requestDetails = {
+            "protocol" : "https:",
+            "hostname" : "api.twilio.com",
+            "method" : "POST",
+            "path" : "/2010-04-01/Accounts/"+config.twilio.accountSid+"Messages.json",
+            "auth" : config.twilio.accountSid+":"+config.twilio.authToken,
+            "headers" : {
+                "Content-Type" : "application/x-www-form-urlencoded",
+                "Content-Length" : Buffer.byteLength(stringPayload)
+            }
+        }
+        var req = https.request(requestDetails,function(res){
+            var status = res.statusCode;
+            if(status == 200 || status == 201){
+                callback(false);
+            }else{
+                callback("Status code returned was "+status);
+            }
+        })
+        req.on("error",function(e){
+            callback(e)
+        })
+        req.write(stringPayload);
+        req.end;
+    }else{
+        callback("Given parameters were missing or invalid")
+    }
+}
+
+
+
+module.exports = helpers
